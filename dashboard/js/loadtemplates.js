9ff2651b778fbe0c526e7e53196f6b3ec8d7b333
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-05-05 06:13:12 +0100

    Pushed

--- dashboard/js/loadtemplates.js
+++ dashboard/js/loadtemplates.js
@@ -1,18 +1,18 @@
-window.addEventListener("load", ()=>{
+// window.addEventListener("load", ()=>{
 
-    const sidebarDiv = document.querySelector("#sidebar-left");
-    const topBarDiv = document.querySelector(".header-right");
+//     const sidebarDiv = document.querySelector("#sidebar-left");
+//     const topBarDiv = document.querySelector(".header-right");
 
-    fetch("http://127.0.0.1:5500/dashboard/shared/sidebar.html")
-    .then(res => res.text())
-    .then(res => {
-        sidebarDiv.innerHTML = res;
-    });
+//     fetch("http://127.0.0.1:5500/dashboard/shared/sidebar.html")
+//     .then(res => res.text())
+//     .then(res => {
+//         sidebarDiv.innerHTML = res;
+//     });
 
-    fetch("http://127.0.0.1:5500/dashboard/shared/topbar.html")
-    .then(res => res.text())
-    .then(res => {
-        topBarDiv.innerHTML = res;
-    });
-})
+//     fetch("http://127.0.0.1:5500/dashboard/shared/topbar.html")
+//     .then(res => res.text())
+//     .then(res => {
+//         topBarDiv.innerHTML = res;
+//     });
+// })
 
